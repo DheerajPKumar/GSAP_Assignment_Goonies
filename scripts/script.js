310866ccc75fc8e023d4ff4c4d398b929581fb44
DheerajPKumar <dheeraj.kumar@axeno.co>
2024-09-07 15:58:01 +0530

    updated the navbar progress.

--- scripts/script.js
+++ scripts/script.js
@@ -108,3 +108,14 @@ function updateBlur() {
 }
 
 window.addEventListener("scroll", updateBlur);
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
